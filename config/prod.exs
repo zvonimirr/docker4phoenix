23a7d5de67d2c7af4f8890a1eb179d990a32fd48
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-01-31 14:15:16 +0100

    Add dynamic host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :docker4phoenix, Docker4phoenixWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST") || "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
